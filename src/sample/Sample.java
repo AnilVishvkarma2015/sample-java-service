f7ed726d7d8fd2bea8094ad9275f003b23159e87
Anil Vishvkarma <anil.vishvkarma@oracle.com>
2023-10-05 14:14:47 +0530

    second commit
    third commit

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,5 +4,15 @@ public class Sample {
 
 	public static void main(String ar[]) {
 		System.out.println("This is the sample Java servcie to demo the Git commands!");
+		pushCommand();
+		pullCommand();
+	}
+
+	public static void pushCommand() {
+		System.out.println("git push origin branch is the command");
+	}
+
+	public static void pullCommand() {
+		System.out.println("git pull origin branch is the command");
 	}
 }
